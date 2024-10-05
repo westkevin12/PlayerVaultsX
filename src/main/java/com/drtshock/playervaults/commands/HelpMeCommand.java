e699d599e701494b7660ccdaccf69fe6964fa77e
mbax <matt@phozop.net>
2024-10-05 15:41:33 -0400

    MC 1.21.1 and up, only

--- src/main/java/com/drtshock/playervaults/commands/HelpMeCommand.java
+++ src/main/java/com/drtshock/playervaults/commands/HelpMeCommand.java
@@ -23,7 +23,6 @@ import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.apache.commons.lang.exception.ExceptionUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -92,7 +91,7 @@ public class HelpMeCommand implements CommandExecutor {
                     try {
                         return new String(Files.readAllBytes(file), StandardCharsets.UTF_8);
                     } catch (IOException e) {
-                        return ExceptionUtils.getFullStackTrace(e);
+                        return e.getMessage();
                     }
                 }
 
