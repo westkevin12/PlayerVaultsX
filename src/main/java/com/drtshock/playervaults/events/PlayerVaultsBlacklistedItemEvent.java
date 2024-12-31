6a4e53556be39b91611c5bed14cc4169edeefba8
Christian W. <7460704+Legoman99573@users.noreply.github.com>
2024-12-31 14:34:48 -0500

    Add blacklist event

---
+++ src/main/java/com/drtshock/playervaults/events/PlayerVaultsBlacklistedItemEvent.java
@@ -0,0 +1,72 @@
+/*
+ * PlayerVaultsX
+ * Copyright (C) 2013 Trent Hensler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.drtshock.playervaults.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
+
+public class PlayerVaultsBlacklistedItemEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+    private boolean isCancelled;
+    private final ItemStack item;
+    private final int vaultNumber;
+    private final Player player;
+
+    public PlayerVaultsBlacklistedItemEvent(Player player, ItemStack item, int vaultNumber) {
+        this.player = player;
+        this.item = item;
+        this.vaultNumber = vaultNumber;
+        this.isCancelled = false;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public ItemStack getItem() {
+        return item;
+    }
+
+    public int getVaultNumber() {
+        return vaultNumber;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.isCancelled = cancelled;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
